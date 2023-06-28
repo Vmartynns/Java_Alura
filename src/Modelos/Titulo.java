7d6ea0e6de4f113ad35bbd9752419cf292dcf080
Vmartynns <vm_martyns@hotmail.com>
2023-06-28 20:37:37 -0300

    Final do curso

--- src/Modelos/Titulo.java
+++ src/Modelos/Titulo.java
@@ -1,11 +1,9 @@
 package Modelos;
 
-import com.google.gson.annotations.SerializedName;
+import Excecao.ConversaoDeAnoException;
 
 public class Titulo implements Comparable<Titulo>{
-    @SerializedName("Title")
     private String nome;
-    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -19,6 +17,11 @@ public class Titulo implements Comparable<Titulo>{
 
     public Titulo(TituloOmdb meuTituloOmdb) {
         this.nome = meuTituloOmdb.title();
+
+        if (meuTituloOmdb.year().length() > 4){
+            throw new ConversaoDeAnoException("Não consegui converter o ano porque tem mais de 4 caracteres");
+        }
+
         this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
         this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0, 2));
     }
@@ -82,7 +85,7 @@ public class Titulo implements Comparable<Titulo>{
 
     @Override
     public String toString() {
-        return "nome= " + nome + ", anoDeLancamento= " + anoDeLancamento + ", duração= " + duracaoEmMinutos ;
+        return "(nome= " + nome + ", anoDeLancamento= " + anoDeLancamento + ", duração= " + duracaoEmMinutos + ")" ;
     }
 
     
--- src/Principal/PrincipalComBusca.java
+++ src/Principal/PrincipalComBusca.java
@@ -1,49 +1,86 @@
 package Principal;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import Excecao.ConversaoDeAnoException;
 import Modelos.Titulo;
 import Modelos.TituloOmdb;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException{
         Scanner leitura = new Scanner(System.in);
-        try {
+        String busca = "";
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder()
+        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+        .setPrettyPrinting()
+        .create();
+
+        while(!busca.equalsIgnoreCase("sair")){
+
             System.out.println("Digite o filme a ser buscado");
-            var busca = leitura.nextLine();
+            busca = leitura.nextLine();
 
-        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=a83d7cd8";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
+            if(busca.equalsIgnoreCase("sair")){
+                break;
+            }
+            
+            String endereco = "http://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=a83d7cd8";
+            
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(endereco))
             .build();
-        HttpResponse<String> response = client
+            HttpResponse<String> response = client
             .send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-        System.out.println(json);
+            String json = response.body();
+            System.out.println(json);
+            
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
 
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
-        
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println("Titulo já convertido");
-        System.out.println(meuTitulo);
-        } 
+            try{
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println("Titulo já convertido");
+                System.out.println(meuTitulo);
+                
+                titulos.add(meuTitulo);
+                
+            } catch(NumberFormatException e ){
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+                
+            } catch(IllegalArgumentException e){
+                System.out.println("Algum erro de argumento na busca, verifique o endereço !");
+            } catch(ConversaoDeAnoException e ) {
+                System.out.println(e.getMessage());
+            }
+            
 
-        finally  {
-            leitura.close();
-        }        
+
+        }
+
+        System.out.println(titulos);
+
+        FileWriter escrita = new FileWriter("filmes.json");
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
+
+        System.out.println("Programa finalizou corretamente");
+            
 
     }
 }
+
